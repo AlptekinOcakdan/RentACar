3cf81046df9f517369f431105276a9b09ea674a6
Alptekin <alptekin.ocakdan@gmail.com>
2022-12-21 18:01:02 +0300

    Day 4

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -8,6 +8,11 @@ using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Business.Concrete {
     public class CarManager : ICarService {
